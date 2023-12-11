75deeb8ac0908ec934f9c7db798989146ece8328
joeyb <jbcassid@mtu.edu>
2023-12-11 16:09:01 -0500

    1.8 done with new statistics compenent

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -8,14 +8,28 @@ const Button = ({ handleClick, text }) => {
   );
 };
 
+const Statistics = ({ good, neutral, bad }) => {
+  let total = good + bad + neutral;
+  let avg = (good - bad) / total;
+  let postitivePer = (good / total) * 100;
+  return (
+    <>
+      <h2>Statistics</h2>
+      <p>good:{good}</p>
+      <p>neutral:{neutral}</p>
+      <p>bad:{bad}</p>
+      <p>total:{total}</p>
+      <p>average:{avg}</p>
+      <p>positive {postitivePer}%</p>
+    </>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  let total = good + bad + neutral;
-  let avg = (good - bad) / total;
-  let postitivePer = (good / total) * 100;
 
   return (
     <div>
@@ -25,14 +39,7 @@ const App = () => {
         text="neutral"
       ></Button>
       <Button handleClick={() => setBad(bad + 1)} text="bad"></Button>
-
-      <h2>Statistics</h2>
-      <p>good:{good}</p>
-      <p>neutral:{neutral}</p>
-      <p>bad:{bad}</p>
-      <p>total:{total}</p>
-      <p>average:{avg}</p>
-      <p>positive {postitivePer}%</p>
+      <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
     </div>
   );
 };
