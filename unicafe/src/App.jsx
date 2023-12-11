68f3317393626a447c8c3f61185c4c0b047eebd7
joeyb <jbcassid@mtu.edu>
2023-12-11 16:05:24 -0500

    1.7 adding statistics done

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -13,6 +13,9 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  let total = good + bad + neutral;
+  let avg = (good - bad) / total;
+  let postitivePer = (good / total) * 100;
 
   return (
     <div>
@@ -27,6 +30,9 @@ const App = () => {
       <p>good:{good}</p>
       <p>neutral:{neutral}</p>
       <p>bad:{bad}</p>
+      <p>total:{total}</p>
+      <p>average:{avg}</p>
+      <p>positive {postitivePer}%</p>
     </div>
   );
 };
