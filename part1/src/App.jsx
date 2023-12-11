49335f998f7b752a8ca3caa79248b094155d53b7
joeyb <jbcassid@mtu.edu>
2023-12-11 15:48:09 -0500

    Star of 1.6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,70 +1,40 @@
-import React from "react";
-const Header = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>{props.course.name}</h1>
-    </div>
-  );
+import { useState } from "react";
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>;
+  }
+
+  return <div>button press history: {props.allClicks.join(" ")}</div>;
 };
 
-const Part = (props) => {
-  console.log(props);
-  return (
-    <>
-      <h1>class:{props.part.name}</h1>
-      <p>Number of exercises: {props.part.exercises}</p>
-    </>
-  );
-};
-
-const Content = (props) => {
-  console.log(props);
-  return (
-    <>
-      {props.classes.map((part) => (
-        <Part key={part.name} part={part}></Part>
-      ))}
-    </>
-  );
-};
-
-const Total = (props) => {
-  var total = 0;
-  props.classes.forEach((course) => {
-    total += course.exercises;
-  });
-  return (
-    <>
-      <h3>Total creadits:{total}</h3>
-    </>
-  );
-};
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
 const App = () => {
-  // const-definitions
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    setLeft(left + 1);
   };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
+  };
+
   return (
     <div>
-      <Header course={course} />
-      <Content classes={course.parts} />
-      <Total classes={course.parts} />
+      {left}
+
+      <Button handleClick={handleLeftClick} text="left" />
+      <Button handleClick={handleRightClick} text="right" />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   );
 };
